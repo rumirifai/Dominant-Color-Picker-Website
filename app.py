a3ec020c1b0d684982d0f0481b3f9b39a896eae2
rumirifai <rumirifai96@gmail.com>
2024-06-10 22:29:45 +0700

    change the main code

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 # Import beberapa library
 import streamlit as st
-import cv2
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from PIL import Image
 
 # Fungsi untuk mendapatkan warna dominan sebanyak K
 def get_dominant_colors(image, k=5):
     # Resize gambar untuk mempercepat proses
-    image = cv2.resize(image, (600, 400))
+    image = image.resize((600, 400))
 
-    # Mengkonversi gambar dari BGR ke RGB
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # Mengkonversi gambar ke dalam bentuk array numpy
+    image_array = np.array(image)
 
     # Reshape gambar ke dalam bentuk (jumlah piksel, 3)
-    image = image.reshape((image.shape[0] * image.shape[1], 3))
+    image = image_array.reshape((-1, 3))
     
     # Menggunakan K-Means untuk menemukan warna dominan
     kmeans = KMeans(n_clusters=k)
@@ -42,13 +42,10 @@ uploaded_file = st.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png'])
 # Kondisi ketika gambar telah di upload
 if uploaded_file is not None:
     # Membaca file yang diunggah
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = Image.open(uploaded_file)
     
-    # Mendekode gambar dengan OpenCV
-    image = cv2.imdecode(file_bytes, 1)
-
     # Menampilkan gambar yang diupload ke website
-    st.image(image, channels="BGR")
+    st.image(image, channels="RGB")
 
     # Mengambil warna dominan
     colors = get_dominant_colors(image)
@@ -56,6 +53,6 @@ if uploaded_file is not None:
     # Teks pada website
     st.write("Dominant Colors:")
 
-    # Membuat dan menampilkan plow warna dominan
+    # Membuat dan menampilkan plot warna dominan
     fig = plot_colors(colors)
     st.pyplot(fig)
